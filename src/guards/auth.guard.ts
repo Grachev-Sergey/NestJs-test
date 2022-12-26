70472a56ad71b1676fa5b83d4e62af991370c9e4
Fusion <fusion@Fusions-Mac-mini.local>
2022-12-26 19:28:31 +0300

    fixed authentication work

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -4,12 +4,17 @@ import type { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
 import { HttpStatus } from '@nestjs/common/enums';
 import { HttpException } from '@nestjs/common/exceptions';
-import { QueryBus } from '@nestjs/cqrs';
-import { GetUserByIdQuery } from 'src/users/queries/impl';
+import { User } from 'src/db/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private queryBus: QueryBus) {}
+  constructor(
+    private jwtService: JwtService,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -20,15 +25,19 @@ export class AuthGuard implements CanActivate {
       if (!token) {
         throw new HttpException('User unauthorized', HttpStatus.UNAUTHORIZED);
       }
-      const payload = this.jwtService.verify(token);
-      const user = this.queryBus.execute(new GetUserByIdQuery(payload.id));
-      if (!user) {
-        throw new HttpException('User unauthorized', HttpStatus.UNAUTHORIZED);
-      }
-      req.user = user;
-      return true;
+      return this.validate(req, token);
     } catch (error) {
       throw new HttpException('User unauthorized', HttpStatus.UNAUTHORIZED);
     }
+  }
+
+  async validate(req, token: string): Promise<boolean> {
+    const payload = this.jwtService.verify(token);
+    const user = await this.userRepository.findOneBy({ id: payload.id });
+    if (!user) {
+      throw new HttpException('User unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+    req.user = user;
+    return true;
   }
 }
